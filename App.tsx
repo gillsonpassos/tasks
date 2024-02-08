18e40219257b70b601c63473221321570b700fb0
gillsonpassos <oliveirapassos07@outlook.com>
2024-02-08 12:53:51 -0300

    Componente lista de tarefas #01

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,7 +29,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -76,8 +76,8 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+          <Section title="Primeiros Passos">
+            Edit <Text style={styles.highlight}>Primeiro Passo</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
