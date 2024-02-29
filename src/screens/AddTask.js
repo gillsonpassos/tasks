11e42d61294c8bd352c28efdf7422ccc9508c99d
gillsonpassos <oliveirapassos07@outlook.com>
2024-02-29 16:23:08 -0300

    Salvar Tarefa

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -24,6 +24,17 @@ export default class AddTask extends Component {
         ...initialState
     }
 
+    save = () => {
+        const newTask = {
+            desc: this.state.desc,
+            date: this.state.date
+        }
+
+        this.props.onSave && this.props.onSave(newTask)
+        this.setState({ ...initialState })
+
+    }
+
     getDatePicker = () => {
         let datePicker = <DateTimePicker value={this.state.date}
             onChange={(_, date) => this.setState({ date, showDatePicker: false })}
@@ -68,7 +79,7 @@ export default class AddTask extends Component {
                         <TouchableOpacity onPress={this.props.onCancel} >
                             <Text style={styles.button} >Cancelar</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={this.save}>
                             <Text style={styles.button} >Salvar</Text>
                         </TouchableOpacity>
                     </View>
