f5dc6d52085cb47e82c2bc820cebd257a2ed2a1c
gillsonpassos <oliveirapassos07@outlook.com>
2024-03-07 15:32:55 -0300

    Integrando Autenticação com Backend

---
+++ src/common.js
@@ -0,0 +1,14 @@
+import { Alert, Platform } from "react-native";
+
+const server = Platform.OS === 'ios'
+    ? 'http://localhost:4000' : 'http://10.0.2.2:4000'
+
+function showError(err) {
+    Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+}
+
+function showSuccess(msg) {
+    Alert.alert('Sucesso!', msg)
+}
+
+export { server, showError, showSuccess }
